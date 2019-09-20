de30148c87ecec57488538239912e7c49749b9a2
Student <farnazardenti@gmail.com>
2019-09-20 19:47:20 +0100

    added more styling, fixed an issue with friends list wall

--- spec/features/add_friends_spec.rb
+++ spec/features/add_friends_spec.rb
@@ -10,26 +10,6 @@ RSpec.feature "Adding friends", type: :feature do
     expect(page).to have_content("Add Friend")
   end
 
-  scenario "Can add a friend from list of users on all posts wall" do
-    sign_up
-    click_link "Logout"
-    sign_up_other_user
-    click_link "All Posts"
-    click_link "Add Friend"
-    expect(page).to have_content("Friends")
-  end
-
-  scenario "Can remove a friend from our own wall" do
-    sign_up
-    click_link "Logout"
-    sign_up_other_user
-    click_link "All Posts"
-    click_link "Add Friend"
-    click_link "My Wall"
-    click_link "Remove Friend"
-    expect(page).to have_content("My Friends 0")
-    expect(page).not_to have_content("My Friends 1")
-  end
 
   scenario "Can add a friend from other users wall from their posts profile name" do
     sign_up
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,14 @@
 require "rails_helper"
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to be }
+
+  it "creates a friendship" do
+    user1 = User.create(email: "friend1@gmail.com", password: "password", password_confirmation: "password")
+    user2 = User.create(email: "friend2@gmail.com", password: "password", password_confirmation: "password")
+    friendship = user1.friendships.build(friend_id: user2.id)
+    expect(friendship.save).to eq true
+    expect(friendship.user_id).to eq user1.id.to_s
+    expect(friendship.friend_id).to eq user2.id.to_s
+  end
 end
